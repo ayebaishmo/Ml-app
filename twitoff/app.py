9dd62123ecd190935a77079694c3e59ca3c677b0
ayebaishmo <ishmo255super@gmail.com>
2024-05-23 23:27:02 +0300

    final commit

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,17 +1,57 @@
 from flask import Flask, render_template
+from .models import DB, User, Tweet
 
 
 # create flask factory
 def create_app():
     app = Flask(__name__)
+
+    # database configuration
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    # register our database with the app
+    DB.init_app(app)
+
     my_var = "Twittoff App"
 
     @app.route('/')
     def root():
+        users = User.query.all()
         return render_template("base.html", title="Home")
 
     @app.route ('/bananas')
     def bananas():
         return render_template("base.html", title="Banana")
     
+    @app.route('/reset')
+    def reset():
+        # Drop all database tables
+        DB.drop_all()
+
+        DB.create_all()
+        return "Database has been reset"
+
+        # Recreate all database tables according to
+        # indicates schema in models.py
+
+    @app.route('/populate')
+    def populate():
+        # create two fake users
+        Ishmo = User(id=1, username='Ishmo')
+        DB.session.add(Ishmo)
+        Julian = User(id=2, username='Julian')
+        DB.session.add(Julian)
+
+        #create two fake tweets on the DB
+        tweet1 = Tweet(id=1, text='Ishmos text', user=Ishmo)
+        DB.session.add(tweet1)
+        tweet2 = Tweet(id=2, text='Julians text', user=Julian)
+        DB.session.add(tweet2)
+
+        # save the changes we just made to the database
+        # "commit" the database changes
+        DB.session.commit()
+
+        return "Database has been populated"
     return app
--- twitoff/models.py
+++ twitoff/models.py
@@ -1,11 +1,31 @@
 # use sqlite
 from flask_sqlalchemy import SQLAlchemy
 
+
 #create a DB object from SQLALchemy
 
 DB = SQLAlchemy()
 
-class User():
-    pass
+class User(DB.Model):
+    # id column
+    id =DB.Column(DB.BigInteger, primary_key=True)
+    # username column
+    username = DB.Column(DB.String, nullable=False)
 
-class Tweet()
+    def __repr__(self):
+        return f"User: {self.username}"
+
+class Tweet(DB.Model):
+    # id column
+    id =DB.Column(DB.BigInteger, primary_key=True)
+    # test column
+    text =DB.Column(DB.Unicode(300))
+    #user_id column (foreign key)
+    user_id = DB.Column(DB.BigInteger, DB.ForeignKey('user.id'), nullable=False)
+    # user column creates  a two way link between user object and Tweet object
+    user = DB.relationship('User', backref=DB.backref('tweets', lazy=True))
+
+
+    def __repr__(self):
+        return f"User: {self.text}"
+    
